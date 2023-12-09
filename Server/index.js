ef0acc8092b4e1d9d5dc50282bd8f34b4e809931
daniel.chidebere <daniel.chidebere@konga.com>
2023-12-09 17:05:14 +0100

    done

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const connectDB = require('./config/connectDB')
+const api = require('./routes/api')
 // Application
 const app = express()
 
@@ -8,6 +9,7 @@ const app = express()
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use("/api", api)
 
 // port 
 const port = process.env.PORT
---
+++ Server/routes/api.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const user = require('./user')
+const app = express()
+
+app.use("/user", user)
+
+module.exports = app
